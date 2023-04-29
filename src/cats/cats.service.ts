541cdf8ddb66a5def0fdc8eec2d0d666654558e0
hg-jang <wkdgusrhkd@gmail.com>
2023-04-29 21:09:12 +0900

    [Feat]: Add cats service methods - create new cat - find all cats - find one cat by id

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Cat } from 'src/types/Cat.type';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+
+  private readonly cats: Cat[] = []
+
+  create(cat: Cat) {
+    this.cats.push(cat)
+  }
+
+  findAll() {
+    return this.cats
+  }
+
+  findOneById(id: number) {
+    return this.cats.find((c) => c.id === id)
+  }
+  
+}
