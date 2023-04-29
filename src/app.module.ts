ba96cd791f87900c1db7e758c1dcb36b5068a24e
hg-jang <wkdgusrhkd@gmail.com>
2023-04-29 20:29:36 +0900

    [Feat]: Add env file path config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const env = process.env.NODE_ENV || 'local';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${env}`,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
